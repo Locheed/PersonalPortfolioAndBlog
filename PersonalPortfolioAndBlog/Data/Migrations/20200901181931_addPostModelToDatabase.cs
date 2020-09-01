cc0328c094ac0a2f9c0f655f8f21cc8694f97a1c
Mika Vesasto <mika.vesasto@saunalahti.fi>
2020-09-01 21:59:35 +0300

    Initial commit

---
+++ PersonalPortfolioAndBlog/Data/Migrations/20200901181931_addPostModelToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersonalPortfolioAndBlog.Data.Migrations
+{
+    public partial class addPostModelToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    PublishDate = table.Column<DateTime>(nullable: false),
+                    FeaturedImage = table.Column<string>(nullable: true),
+                    Excerpt = table.Column<string>(nullable: true),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ PersonalPortfolioAndBlog/Models/PostModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalPortfolioAndBlog.Models
+{
+    public class PostModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string FeaturedImage { get; set; }
+        public string Excerpt { get; set; }
+        public string Content { get; set; }
+    }
+}
